a2f2e4504236d6226c401e8d0e4ec726f0b0ca0d
chenzhe142 <chenzhe142@163.com>
2015-06-13 00:14:27 -0500

    update admin interface

--- polls/admin.py
+++ polls/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
+from .models import Question, Choice
+
 # Register your models here.
+
+class QuestionAdmin(admin.ModelAdmin):
+	"""docstring for QuestionAdmin"""
+	fields = ['pub_date', 'question_text']
+	
+
+# add admin interface to Question table
+# update the order of items
+admin.site.register(Question, QuestionAdmin)
+admin.site.register(Choice)
